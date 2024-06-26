8deb83f310ae7fa24189738e83ec34d8bb2a52ef
Matheus Cruz <165737916+rlxmts@users.noreply.github.com>
2024-06-26 15:33:17 -0300

    :iphone: Responsividade: Site responsivo

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,43 @@
 const cardImage = document.querySelectorAll('.card_img');
+const cabecalho = document.querySelector('.cabecalho');
+const pergunta = document.querySelectorAll('.pergunta');
 
 cardImage.forEach( card => {
     const pointer = card.querySelector('span');
 })
 
-function addOuRemoveAtivo(element){
-    element.classList.contains('ativo') ?  element.classList.remove('ativo') : element.classList.add('ativo');    
-} 
-const pergunta = document.querySelectorAll('.pergunta');
-
 pergunta.forEach( pergunta => {
     pergunta.onclick =()=> {
-        addOuRemoveAtivo(pergunta);
+        addOuRemoveAtivo(pergunta, 'ativo');
         pergunta.classList.contains('ativo') ? pergunta.classList.remove('inativo') : pergunta.classList.add('inativo');
     }
 })
+
+function addOuRemoveAtivo(element, classe){
+    element.classList.contains(`${classe}`) ?  element.classList.remove(`${classe}`) : element.classList.add(`${classe}`);    
+} 
+
+let posicaoAnt = window.scrollY;
+
+function escondeCabecalho(){
+
+        let posicaoAtual = window.scrollY;
+
+        if(posicaoAnt < posicaoAtual){
+            cabecalho.style.top = '-100%';
+        }else{
+            cabecalho.style.top = '0';
+        }
+
+        posicaoAnt = posicaoAtual;
+}
+
+window.addEventListener('scroll', ()=> {
+    addClasseCabecalho();
+    escondeCabecalho();
+})
+
+function addClasseCabecalho(){
+    const posicaoScroll = window.scrollY;
+    posicaoScroll > 0 ? cabecalho.classList.add('cabecalho-ativo') : cabecalho.classList.remove('cabecalho-ativo');
+}
