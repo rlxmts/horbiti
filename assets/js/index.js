51d97563d8f609c4cdd7fab256567cb64bac3e88
Matheus Cruz <165737916+rlxmts@users.noreply.github.com>
2024-06-26 17:48:39 -0300

    :sparkles: Add bota wpp e bt subir ao topo

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,43 +1,46 @@
-const cardImage = document.querySelectorAll('.card_img');
 const cabecalho = document.querySelector('.cabecalho');
 const pergunta = document.querySelectorAll('.pergunta');
+const iconSubir = document.querySelector('.icon');
 
-cardImage.forEach( card => {
-    const pointer = card.querySelector('span');
-})
-
-pergunta.forEach( pergunta => {
-    pergunta.onclick =()=> {
-        addOuRemoveAtivo(pergunta, 'ativo');
-        pergunta.classList.contains('ativo') ? pergunta.classList.remove('inativo') : pergunta.classList.add('inativo');
-    }
-})
-
-function addOuRemoveAtivo(element, classe){
+function addOuRemoveClasse(element, classe){
     element.classList.contains(`${classe}`) ?  element.classList.remove(`${classe}`) : element.classList.add(`${classe}`);    
 } 
 
 let posicaoAnt = window.scrollY;
 
-function escondeCabecalho(){
-
-        let posicaoAtual = window.scrollY;
-
-        if(posicaoAnt < posicaoAtual){
-            cabecalho.style.top = '-100%';
-        }else{
-            cabecalho.style.top = '0';
-        }
-
-        posicaoAnt = posicaoAtual;
-}
-
 window.addEventListener('scroll', ()=> {
     addClasseCabecalho();
     escondeCabecalho();
+    ativaBotaoSubir();
 })
 
 function addClasseCabecalho(){
     const posicaoScroll = window.scrollY;
     posicaoScroll > 0 ? cabecalho.classList.add('cabecalho-ativo') : cabecalho.classList.remove('cabecalho-ativo');
 }
+
+function ativaBotaoSubir(){
+    window.scrollY > 900 ? iconSubir.classList.add('icon-ativo') : iconSubir.classList.remove('icon-ativo');
+}
+
+function escondeCabecalho(){
+
+    let posicaoAtual = window.scrollY;
+
+    if(posicaoAnt < posicaoAtual){
+        cabecalho.style.top = '-100%';
+    }else{
+        cabecalho.style.top = '0';
+    }
+
+    posicaoAnt = posicaoAtual;
+}
+
+pergunta.forEach( pergunta => {
+    pergunta.onclick =()=> {
+        addOuRemoveClasse(pergunta, 'ativo');
+        pergunta.classList.contains('ativo') ? pergunta.classList.remove('inativo') : pergunta.classList.add('inativo');
+    }
+})
+
+iconSubir.onclick = ()=> window.scrollTo(0, 0);
