c0a3eb22e9a5791891378954b2f2eb4d1cafbe31
Matheus Cruz <165737916+rlxmts@users.noreply.github.com>
2024-06-26 13:45:17 -0300

    :lipstick: Estilizando Seção Duvidas

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,14 +2,16 @@ const cardImage = document.querySelectorAll('.card_img');
 
 cardImage.forEach( card => {
     const pointer = card.querySelector('span');
-    card.onmousemove = ()=> {
-        pointer.style.display = 'none';
-    }
+})
 
-    card.ontouchmove = ()=> {
-        pointer.styled.display ='none';
-    }
-    card.onmouseout = ()=> {
-        pointer.style.display = 'grid';
+function addOuRemoveAtivo(element){
+    element.classList.contains('ativo') ?  element.classList.remove('ativo') : element.classList.add('ativo');    
+} 
+const pergunta = document.querySelectorAll('.pergunta');
+
+pergunta.forEach( pergunta => {
+    pergunta.onclick =()=> {
+        addOuRemoveAtivo(pergunta);
+        pergunta.classList.contains('ativo') ? pergunta.classList.remove('inativo') : pergunta.classList.add('inativo');
     }
 })
